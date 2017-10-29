6fc3019379410462ed974dfdbc25476824f54e5d
Andreas Schröpfer <andreas@apxp.info>
2017-10-29 17:33:37 +0100

    List func korrigiert

--- 01_Grundlagen/060_methoden/01_uebung/02_linked_list/list.go
+++ 01_Grundlagen/060_methoden/01_uebung/02_linked_list/list.go
@@ -6,19 +6,19 @@ type Element struct {
 	prev  *Element
 }
 
-func (e *Element) Next() { return e }
+func (e *Element) Next() *Element { return e }
 
-func (e *Element) Prev() { return e }
+func (e *Element) Prev() *Element { return e }
 
 type List struct {
 	currentElement *Element
 	head           *Element
 }
 
-func (l List) Front() *Element { return nil }
+func (l *List) Front() *Element { return nil }
 
-func (l List) AddAt(i int, v string) error {}
+func (l *List) AddAt(i int, v string) error { return nil }
 
-func (l List) Get(i int) (*Element, error) { return nil }
+func (l *List) Get(i int) (*Element, error) { return nil, nil }
 
-func (l List) Len() int { return 0 }
+func (l *List) Len() int { return 0 }
--- 01_Grundlagen/060_methoden/01_uebung/02_linked_list/list_test.go
+++ 01_Grundlagen/060_methoden/01_uebung/02_linked_list/list_test.go
@@ -3,7 +3,7 @@ package list
 import "testing"
 
 func TestList(t *testing.T) {
-	l := List{}
+	l := &List{}
 	values := []string{"null", "eins", "zwei", "drei"}
 	for i, v := range values {
 		err := l.AddAt(i, v)
@@ -16,8 +16,8 @@ func TestList(t *testing.T) {
 		if err != nil {
 			t.Error("no error expected", err)
 		}
-		if got != v {
-			t.Errorf("at idnex %d exp: %s got %s", i, v, got)
+		if got.Value != v {
+			t.Errorf("at idnex %d exp: %s got %s", i, v, got.Value)
 		}
 	}
 }
